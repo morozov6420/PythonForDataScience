c2a11843559ca1a32c87f8800875d866dbfc47c1
Aleksei Morozov <morozov6420@gmail.com>
2020-06-21 15:13:55 +0500

    gitignore

--- day2_19.06/homework/homework_19_06.py
+++ day2_19.06/homework/homework_19_06.py
@@ -6,11 +6,11 @@
 import numpy as np
 # Создание массива из int
 np.random.seed(42)
-x = np.random.randint(5, size = (5,5)) # массив 5 на 5 с элементами от 0 до 4
+x = np.random.randint(5, size=(5, 5))  # массив 5 на 5 с элементами от 0 до 4
 # Сохранение массива в файл
-np.savetxt('day2_19.06/homework/matrix.csv', x, fmt = '%i', delimiter = ',')
+np.savetxt('day2_19.06/homework/matrix.csv', x, fmt='%i', delimiter=',')
 # Чтение массива из файла
-matrix = np.loadtxt('day2_19.06/homework/matrix.csv', delimiter = ',')
+matrix = np.loadtxt('day2_19.06/homework/matrix.csv', delimiter=',')
 # Смотрим, какие элементы максимальные и минимальные
 max_id = matrix == matrix.max()
 min_id = matrix == matrix.min()
