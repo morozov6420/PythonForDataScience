33d95216b0c999cdd5b52e5e1bdc7309facfc3ec
Aleksei Morozov <morozov6420@gmail.com>
2020-06-25 08:21:04 +0500

    day3 удалил лишную запись в файл

--- day3_24.06/homework/data_extraction.py
+++ day3_24.06/homework/data_extraction.py
@@ -8,22 +8,21 @@ images = []
 with zipfile.ZipFile('day3_24.06/homework/letters.zip', 'r') as zf:
     infoList = zf.infolist()
     nameList = zf.namelist()
-    with open('day3_24.06/homework/letters.csv', "w") as letters:
-        for ind, file in enumerate(infoList):
-            imageFile = zf.open(file)
-            lst = np.array(Image.open(imageFile).convert("L")).ravel().tolist()
-            if len(lst) != 1024:
-                images.append(
-                    lst +
-                    [0]*32 + 
-                    # В датасете косяк, 4 фотки отличаются на 1 пиксель
-                    [int(nameList[ind][:2])]
-                )
-            else:
-                images.append(
-                    lst +
-                    [int(nameList[ind][:2])]
-                )
+    for ind, file in enumerate(infoList):
+        imageFile = zf.open(file)
+        lst = np.array(Image.open(imageFile).convert("L")).ravel().tolist()
+        if len(lst) != 1024:
+            images.append(
+                lst +
+                [0]*32 + 
+                # В датасете косяк, 4 фотки отличаются на 1 пиксель
+                [int(nameList[ind][:2])]
+            )
+        else:
+            images.append(
+                lst +
+                [int(nameList[ind][:2])]
+            )
     print('Done')
 
 np.array(images)
