c20fa9f5306a0ad3377fc77ed47cff02b11bb6c1
Aleksei Morozov <morozov6420@gmail.com>
2020-06-25 05:38:56 +0500

    day3 дописал лекцию

--- day3_24.06/lecture/lecture.py
+++ day3_24.06/lecture/lecture.py
@@ -1,3 +1,4 @@
+from scipy import ndimage
 from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
@@ -77,37 +78,41 @@ mat = np.array([
     [0,          0,           1]
 ])
 
-plt.imshow(transform(pix=pix, mat=mat))
+new_pix = transform(pix=pix, mat=mat)
+plt.imshow(new_pix)
+plt.show()
+# Сохраним результаты
+plt.imsave('day3_24.06/lecture/1_rotated.png', new_pix)
+
+# Встроенные инструменты для работы с изображениями
+# Чтение изображения в формате массива
+color_pix = plt.imread('day3_24.06/lecture/1.png')
+plt.imshow(color_pix)
 plt.show()
 
+# Поворот изображения
+rotated_pix = ndimage.rotate(
+    color_pix,
+    10,
+    reshape=-1
+)  # Получили то же самое, что сделал я
+plt.imshow(rotated_pix)
+plt.show()
 
-# fi = np.radians(10)
-# mat = np.array([
-#     [np.cos(fi), -np.sin(fi), 0],
-#     [np.sin(fi), np.cos(fi),  0],
-#     [0,          0,           1]
-# ])
-# tmp = []
-# for x in range(pix.shape[0]):
-#     for y in range(pix.shape[1]):
-#         tmp.append(np.array(mat @ [x, y, 0]).astype('int'))
+# Преобразование собеля
+# Выделение границ объектов
+sob_pix = ndimage.sobel(color_pix)
+plt.imshow(sob_pix)
+plt.show()
 
-# tmp = np.delete(tmp, 2, 1)
-# x_min = tmp[:, 0].min()
-# x_max = tmp[:, 0].max()
-# y_min = tmp[:, 1].min()
-# y_max = tmp[:, 1].max()
-# print(x_min, x_max, y_min, y_max)
+# Вырезание части изображения с помощью слайсов
+lx, ly, _ = color_pix.shape
+corp_pix = color_pix[lx // 4: - lx // 4,
+                     ly // 4: - ly // 4]
+plt.imshow(corp_pix)
+plt.show()
 
-# new_tmp = np.vstack((tmp[:, 0] - x_min, tmp[:, 1] - y_min)).T
-# new_pix = np.zeros(shape=(abs(x_min) + abs(x_max) +
-#                           1, abs(y_min) + abs(y_max) + 1))
-# i = 0
-# for x in range(pix.shape[0]):
-#     for y in range(pix.shape[1]):
-#         new_pix[new_tmp[i, 0], new_tmp[i, 1]] = pix[x, y]
-#         i += 1
-
-
-# plt.imshow(new_pix)
-# plt.show()
+# Размытие с помощью функции Гаусса
+blur_pix = ndimage.gaussian_filter(color_pix, sigma=10)
+plt.imshow(blur_pix)
+plt.show()
